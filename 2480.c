070f4952eaa3f99e1b61b9e7e5491202f2d20a61
MomisAlien <127112536+momisAlien@users.noreply.github.com>
2023-03-31 13:15:06 +0900

    Update 2480.c

--- 2480.c
+++ 2480.c
@@ -2,33 +2,36 @@
 
 int main(void) {
 	int a, b, c;  {
-		scanf("%d %d %d",&a, &b, &c);
-		0 < a, b, c&& a, b, c < 7;
-		if(a==b&&b==c&&c==a)
+		scanf("%d %d %d",&a, &b, &c); // 주사위 3개 변수 지정
+		0 < a, b, c&& a, b, c < 7; // 주사위 1~6까지 범위 지정
+		if(a==b&&b==c&&c==a) // 1) 주사위 3개가 모두 같은 수 
 		{
 			printf("%d", 10000+a*1000);
 		}
-		else if (a != b && a != c && b != c)
+		else if (a != b && a != c && b != c) // 주사위 3개가 모두 다 다른 수 
 		{
-			if(a > b && a > c)
+			if(a > b && a > c) // 1) a가 가장 큰 경우 (b랑 c의 대소 불필요)
 			{
 				printf("%d", a * 100);
 			}
-			else if (b > a && b > c)
+			else if (b > a && b > c) // 2) b가 가장 큰 경우 (a랑 c의 대소 불필요) 
 			{
 				printf("%d", b * 100);
 			}
-			else {
-				printf("%d", c * 100);
+			else {                         // else 쓴 이유 : a,b,c 모두가 다른 수 이기 때문에 남은 경우는 c가 가장 큰 경우 뿐
+				printf("%d", c * 100); // 3) c가 가장 큰경우 a,b 의 대소 불필요, 
 			}
 			
-		}if (a == b && a != c) {
+		}if (a == b && a != c) // a=b인 경우 
+		{
 			printf("%d",a*100+1000);
 		}
-		else if (a == c && a != b) {
+		else if (a == c && a != b) // a=c인 경우
+		{
 			printf("%d",a*100+1000);
 		}
-		else if (b == c && b != a) {
+		else if (b == c && b != a) // b=c인 경우
+		{
 			printf("%d",b*100+1000);
 		}
 	}
